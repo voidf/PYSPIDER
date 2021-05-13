a6d5664c8268fffc9d94a0cd323e3ecc32d74856
Kaguya <kaguya@Kaguya.cn>
2021-05-14 00:56:53 +0800

    done

--- server.py
+++ server.py
@@ -12,20 +12,34 @@ import json
 from mongoengine import *
 from decimal import Decimal
 
+from io import BytesIO
+
 
 connect('DOGECOIN_MONITOR', host='localhost', port=27017)
 
-class Stock(Document):
-    dat = DateTimeField()
-    val = DecimalField(precision=20)
+# class Stock(Document):
+#     dat = DateTimeField()
+#     val = DecimalField(precision=20)
     
 
 class BTCHisInfo(Document):
-    dat = DateTimeField()
+    date = DateTimeField(primary_key=True)
+    # date = DateTimeField()
     open = DecimalField(precision=20)
     close = DecimalField(precision=20)
     high = DecimalField(precision=20)
     low = DecimalField(precision=20)
+    volume = StringField()
+    def get_dict(self):
+        return {
+            'date':self.date,
+            'open':self.open,
+            'close':self.close,
+            'high':self.high,
+            'low':self.low,
+            'volume':self.volume
+        }
+    
 
 app = FastAPI()
 
@@ -38,36 +52,12 @@ tracecfg = aiohttp.TraceConfig()
 tracecfg.on_request_start.append(on_request_start)
 tracecfg.on_request_end.append(on_request_end)
 
-async def make_req(dvd:int):
-    ima = datetime.datetime.now()
-    prv = ima - datetime.timedelta(days=dvd)
-    async with aiohttp.ClientSession(trace_configs=[tracecfg]) as s:      
-        async with s.get(
-            f'''https://api.nasdaq.com/api/quote/DOGE/chart?assetclass=crypto&fromdate={prv.strftime('%Y-%m-%d')}&todate={ima.strftime('%Y-%m-%d')}''',
-            headers={
-                "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-                "accept-encoding":"gzip, deflate, br",
-                "accept-language":"zh-CN,zh;q=0.9",
-                "cache-control":"no-cache",
-                "dnt":"1",
-                "pragma":"no-cache",
-                "sec-fetch-mode":"navigate",
-                "sec-fetch-site":"none",
-                "sec-fetch-user":"?1",
-                "upgrade-insecure-requests":"1",
-                "user-agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
-            }
-        ) as r:
-            res = await r.text()
-            print(json.loads(res))
-        return res
-
 async def make_req2(dvd: int):
     ima = datetime.datetime.now()
     prv = ima - datetime.timedelta(days=dvd)
     async with aiohttp.ClientSession(trace_configs=[tracecfg]) as s:      
         async with s.get(
-            f'''https://api.nasdaq.com/api/quote/BTC/historical?assetclass=crypto&fromdate={prv.strftime('%Y-%m-%d')}&limit={dvd}8&todate={ima.strftime('%Y-%m-%d')}''',
+            f'''https://api.nasdaq.com/api/quote/BTC/historical?assetclass=crypto&fromdate={prv.strftime('%Y-%m-%d')}&limit={dvd}&todate={ima.strftime('%Y-%m-%d')}''',
             headers={
                 "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
                 "accept-encoding":"gzip, deflate, br",
@@ -87,64 +77,119 @@ async def make_req2(dvd: int):
         return res
     
 
-def save_and_get_len(res:str):
-    for i in json.loads(res)["data"]['chart']:
-        d = datetime.datetime.strptime(i['z']['dateTime'], "%m/%d/%Y")
-        print(d)
-        pre = Stock.objects(dat=d)
-        if pre:
-            pre.delete()
-
-        Stock(
-            dat=d,
-            val=Decimal(i['z']['value'])
-        ).save()
-    return len(Stock.objects())
-
-
 import matplotlib.pyplot as plt
-import mpl_finance as mpf
+import matplotlib
+import pandas
+from cycler import cycler
+import mplfinance as mpf
 from matplotlib.pylab import date2num
+import numpy
 
-@app.get('/{prv}')
+
+async def sketch(s: dict) -> BytesIO:
+    df = pandas.DataFrame.from_dict(s)
+    df["datetime"] = pandas.to_datetime(df['date'])
+    df.set_index("datetime",inplace=True)
+    df.sort_index(ascending=True, inplace=True)
+    df.drop(["date"], axis=1,inplace=True)
+    df.drop(["volume"], axis=1,inplace=True)
+    df[['open','close','high','low']] = df[['open','close','high','low']].astype('float64')
+    # df[['volume']] = 0
+    matplotlib.rcParams['axes.prop_cycle']=cycler(
+        color=['dodgerblue', 'deeppink', 
+        'navy', 'teal', 'maroon', 'darkorange', 
+        'indigo']
+    )
+
+    matplotlib.rcParams['lines.linewidth']=.5
+
+    buf = BytesIO()
+    # kwa = {"savefig":b}
+
+    mpf.plot(
+        df,
+        type='candle',
+        mav=(7, 30, 60), 
+        # volume=True, 
+        title='BTC historical',
+        ylabel='USD $', 
+        # ylabel_lower='Shares\nTraded Volume', 
+        figratio=(15, 10), 
+        figscale=2.0,
+        style=mpf.make_mpf_style(
+            gridaxis='both',
+            gridstyle='-.',
+            y_on_right=False,
+            marketcolors=mpf.make_marketcolors(
+                up='red',
+                down='green',
+                edge='i',
+                wick='i',
+                # volume='in',
+                inherit=True
+            )
+        ),
+        # **kwa
+        savefig=buf
+    )
+    # print(buf)
+    buf.seek(0)
+    # print(buf.read())
+    
+    return buf
+
+# @app.options('/master')
+# async def masteroptions():
+#     return Response(None, headers={
+#         'Access-Control-Allow-Origin': '*',
+#         "Access-Control-Allow-Methods": "POST, GET, PUT, OPTIONS, DELETE",
+#         "Access-Control-Allow-Headers": "Access-Control-Allow-Origin"
+#     })
+
+@app.get('/master')
 async def master(prv: int):
     s = ujson.loads(await make_req2(prv))['data']['tradesTable']['rows']
-    li = []
-    for raw in s:
-        _date = datetime.datetime.strptime(raw['date'], "%m/%d/%Y")
-        _close = Decimal(raw['close'])
-        _open = Decimal(raw['open'])
-        _high = Decimal(raw['high'])
-        _low = Decimal(raw['low'])
-        li.append((date2num(_date), _open, _high, _low, _close))
-    print(*li)
-    fig, ax = plt.subplots()
-    fig.subplots_adjust(bottom=0.2)
-    ax.xaxis_date()
-    plt.yticks()
-    plt.xticks(rotation=45)
-    plt.xlabel("History")
-    plt.ylabel("USD")
+    # print(s)
+    # print(type(s))
+    pic = await sketch(s)
+    return Response(pic.read(), media_type="image/png", headers={
+        'Access-Control-Allow-Origin': '*',
+        "Access-Control-Allow-Methods": "POST, GET, PUT, OPTIONS, DELETE",
+        "Access-Control-Allow-Headers": "Access-Control-Allow-Origin"
+    })
 
-    mpf.candlestick_ohlc(ax, li, width=1.5, colorup='r', colordown='green')
+@app.get('/save')
+async def save_1(prv: int):
+    print(prv)
+    # res = await make_req2(prv)
+    # BTCHisInfo.pk = 'date'
+    BTCHisInfo.objects().delete()
+    for i in ujson.loads(await make_req2(prv))['data']['tradesTable']['rows']:
+        # BTCHisInfo.objects(date=datetime.datetime.strptime(i['date'], "%m/%d/%Y")).delete()
+        BTCHisInfo(**i).save()
+    return Response(str(len(BTCHisInfo.objects())), headers={
+        'Access-Control-Allow-Origin': '*',
+        "Access-Control-Allow-Methods": "POST, GET, PUT, OPTIONS, DELETE",
+        "Access-Control-Allow-Headers": "Access-Control-Allow-Origin"
+    })
 
-    plt.grid()
-
-    plt.savefig('a.png')
-
-    return li
-    
-    # return save_and_get_len(await make_req(prv))
-
-@app.get('/draw')
-async def draw_(prv: int):
-    res = await make_req(prv)
-    for i in json.loads(res)["data"]['chart']:
-        d = datetime.datetime.strptime(i['z']['dateTime'], "%m/%d/%Y")
-        print(d)
+@app.get('/load')
+async def load_1():
+    # print(BTCHisInfo.objects().to_json())
+    # j = ujson.loads(BTCHisInfo.objects().to_json())
+    # print(j)
+    pic = await sketch([j.get_dict() for j in BTCHisInfo.objects()])
+    return Response(pic.read(), media_type="image/png", headers={
+        'Access-Control-Allow-Origin': '*',
+        "Access-Control-Allow-Methods": "POST, GET, PUT, OPTIONS, DELETE",
+        "Access-Control-Allow-Headers": "Access-Control-Allow-Origin"
+    })
 
 
 
 
 if __name__ == '__main__':
     uvicorn.run(app)
+
+
+# https://medium.com/swlh/building-an-amazing-cross-platform-pok%C3%A9dex-desktop-application-with-electron-react-and-5ed997a5c0f1
--- src/compo/Master.jsx
+++ src/compo/Master.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import {AppBar, Toolbar, Grid, Card, CardContent, CircularProgress, CardMedia, Typography, TextField, Button} from "@material-ui/core"
+import {Box, AppBar, Toolbar, Grid, Card, CardContent, CircularProgress, CardMedia, Typography, TextField, Button, Slider} from "@material-ui/core"
 import { makeStyles, fade } from '@material-ui/core/styles';
 import axios from "axios"
 import { useState } from 'react';
 import { useEffect } from 'react';
 import SearchIcon from "@material-ui/icons/Search"
+import ReactDom from 'react-dom';
 import { toFirstCharUpperCase } from "../utils/constants";
 import SaveIcon from '@material-ui/icons/Save';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import EjectIcon from '@material-ui/icons/Eject';
 
 const useStyles = makeStyles(theme => ({
     pokedexContainer: {
@@ -38,17 +41,18 @@ const useStyles = makeStyles(theme => ({
     }
 }))
 
-
+const baseurl = "http://rinko.work:7012/"
+const baseurlocal = "http://localhost:8000/"
 
 const Master = (props) => {
 
     const {history} = props
-    const [pokemonData,setPokemonData] = useState()
+    const [ppt, setppt] = useState(30)
     const [filter,setFilter] = useState("")
-
+    
     const classes = useStyles()
+    
 
-    // const initButtons = 
 
     return (
         <>
@@ -57,13 +61,131 @@ const Master = (props) => {
                 
                 <Button 
                     variant="contained" 
-                    color="default"
+                    color="secondary"
+                    startIcon={<EjectIcon />}
+                    size="large"
+                    style={{
+                        color:'#FFF',
+                        width:'180px',
+                        margin:5
+                    }}
+                    onClick = {()=>{
+                        const uurl = baseurl+"load?prv="+ppt
+                        const a =(
+                            <>
+                                <Typography variant="h1">
+                                    <img src={uurl} alt="" height="100%" width="100%" />
+                                </Typography>
+
+                            </>
+                        );
+                        ReactDom.render(a, document.querySelector("#ASDF"))
+                    }}
+                >读库</Button>
+
+                <Button 
+                    variant="contained" 
+                    color="secondary"
                     startIcon={<SaveIcon />}
                     size="large"
-                    onClick = {()=>{alert("??")}}
-                >爬</Button>
+                    style={{
+                        color:'#FFF',
+                        width:'180px',
+                        margin:5
+                    }}
+                    onClick = {()=>{
+                        axios.get(baseurl+"save?prv="+ppt).then(
+                            resp=>{
+                                console.log(resp);
+                                alert("存库成功，当前记录数："+resp.data);
+                            }
+                        )
+                        // alert()
+                    }}
+                >入库</Button>
+
+                <Button 
+                    variant="contained" 
+                    color="secondary"
+                    startIcon={<ArrowDownwardIcon />}
+                    size="large"
+                    style={{
+                        color:'#FFF',
+                        width:'180px',
+                        margin:5
+                    }}
+                    onClick = {()=>{
+                        const uurl = baseurl+"master?prv="+ppt;
+                        
+                        // axios.get("http://localhost:8000/master?prv="+ppt,
+                        // {
+                        //     headers:{
+                        //         'Access-Control-Allow-Origin':'*'
+                        //     },
+                        //     responseType:'arraybuffer'
+                        // }).then((response)=>{
+                        //         return 'data:image/png;base64,' +btoa(new Uint8Array(response.data).reduce((data, byte) => data + String.fromCharCode(byte), ''));
+                        //     }).then(data=>{
+                        //         const a =(
+                        //             <>
+                        //                 <Typography variant="h1">
+                        //                     <img src={data} alt="" />
+                        //                 </Typography>
+        
+                        //             </>
+                        //         );
+                        //         ReactDom.render(a, document.querySelector("#ASDF"))
+                        //     })
+                            const a =(
+                                <>
+                                    <Typography variant="h1">
+                                        <img src={uurl} alt="" height="100%" width="100%" />
+                                    </Typography>
+    
+                                </>
+                            );
+                            ReactDom.render(a, document.querySelector("#ASDF"))
+
+                        
+                    }}
+                >直接爬</Button>
+                <Box style={{margin:"10px",width:"100%"}}>
+                    <Typography id="discrete-slider" gutterBottom>
+                        需要爬前多少天的数据？
+                    </Typography>
+
+                    <Slider
+                        id="MX"
+                        defaultValue={30}
+                        aria-labelledby="discrete-slider"
+                        valueLabelDisplay="auto"
+                        step={10}
+                        marks
+                        min={10}
+                        max={360}
+                        color="secondary"
+                        onChange={(e,val)=>{setppt(val)}}
+                        
+                    />
+                </Box>
+
+
+                   
+                    {/* <TextField 
+                        id="standard-basic" 
+                        label="需要爬前多少天的数据？"
+                        
+                        fullWidth
+                        style={{
+                            margin:15
+                        }}
+                        color="secondary"
+                    ></TextField> */}
             </Toolbar>
         </AppBar>
+        <Card id="ASDF" style={{height:"100%"}}>
+
+        </Card>
             
         </>
     );
