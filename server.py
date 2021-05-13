deb9454673ee3979812f412cfc5e2a48a0c20908
ATRI <A@T.RI>
2021-05-13 09:07:08 +0800

    mmp

--- server.py
+++ server.py
@@ -20,6 +20,13 @@ class Stock(Document):
     val = DecimalField(precision=20)
     
 
+class BTCHisInfo(Document):
+    dat = DateTimeField()
+    open = DecimalField(precision=20)
+    close = DecimalField(precision=20)
+    high = DecimalField(precision=20)
+    low = DecimalField(precision=20)
+
 app = FastAPI()
 
 async def on_request_start(session, trace_config_ctx, params):
@@ -53,23 +60,76 @@ async def make_req(dvd:int):
         ) as r:
             res = await r.text()
             print(json.loads(res))
+        return res
 
-            for i in json.loads(res)["data"]['chart']:
-                d = datetime.datetime.strptime(i['z']['dateTime'], "%m/%d/%Y")
-                print(d)
-                pre = Stock.objects(dat=d)
-                if pre:
-                    pre.delete()
+async def make_req2(dvd: int):
+    ima = datetime.datetime.now()
+    prv = ima - datetime.timedelta(days=dvd)
+    async with aiohttp.ClientSession(trace_configs=[tracecfg]) as s:      
+        async with s.get(
+            f'''https://api.nasdaq.com/api/quote/BTC/historical?assetclass=crypto&fromdate={prv.strftime('%Y-%m-%d')}&limit=18&todate={ima.strftime('%Y-%m-%d')}''',
+            headers={
+                "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+                "accept-encoding":"gzip, deflate, br",
+                "accept-language":"zh-CN,zh;q=0.9",
+                "cache-control":"no-cache",
+                "dnt":"1",
+                "pragma":"no-cache",
+                "sec-fetch-mode":"navigate",
+                "sec-fetch-site":"none",
+                "sec-fetch-user":"?1",
+                "upgrade-insecure-requests":"1",
+                "user-agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+            }
+        ) as r:
+            res = await r.text()
+            print(json.loads(res))
+        return res
+    
 
-                Stock(
-                    dat=d,
-                    val=Decimal(i['z']['value'])
-                ).save()
-            return len(Stock.objects())
+def save_and_get_len(res:str):
+    for i in json.loads(res)["data"]['chart']:
+        d = datetime.datetime.strptime(i['z']['dateTime'], "%m/%d/%Y")
+        print(d)
+        pre = Stock.objects(dat=d)
+        if pre:
+            pre.delete()
 
-@app.get('/')
-async def master():
-    return await make_req(8)
+        Stock(
+            dat=d,
+            val=Decimal(i['z']['value'])
+        ).save()
+    return len(Stock.objects())
+
+
+@app.get('/{prv}')
+async def master(prv: int):
+    s = ujson.loads(await make_req2(prv))['data']['tradesTable']['rows']
+    li = []
+    for raw in s:
+        _date = datetime.datetime.strptime(raw['date'], "%m/%d/%Y")
+        _close = Decimal(raw['close'])
+        _open = Decimal(raw['open'])
+        _high = Decimal(raw['high'])
+        _low = Decimal(raw['low'])
+        li.append((_date, _open, _close, _high, _low))
+    print(*li)
+    return li
+    
+    # return save_and_get_len(await make_req(prv))
+
+import matplotlib.pyplot as plt
+from matplotlib.pylab import date2num
+
+@app.get('/draw')
+async def draw_(prv: int):
+    res = await make_req(prv)
+    for i in json.loads(res)["data"]['chart']:
+        d = datetime.datetime.strptime(i['z']['dateTime'], "%m/%d/%Y")
+        print(d)
+
+
+
 
 if __name__ == '__main__':
     uvicorn.run(app)
